2e0a98922ee99e7eb736d2205abe664eab88f5f9
Charles Ellison <crelliso@gmail.com>
2024-05-25 17:57:09 -0500

    Create shared types
    * Add jest.config.ts
    * Add app.ts
    * Add server.ts
    * Create shared types

---
+++ server/server.ts
@@ -0,0 +1,24 @@
+import { GraphQLSchema } from "graphql";
+import { RootQuery } from "./server/graphql/queries";
+import { app } from "./server/app";
+import { graphqlHTTP } from "express-graphql";
+
+const express = app;
+
+const schema = new GraphQLSchema({
+  query: RootQuery,
+});
+
+express.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+express.listen({ port: 4000 }, () => {
+  console.log(
+    `��� Running a GraphQL API server at http://localhost:4000/graphql`
+  );
+});
