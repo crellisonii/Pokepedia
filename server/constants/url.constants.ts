d11685bd3b0ad6e1f08e3fc439d8c4e22b6c2906
Charles Ellison <crelliso@gmail.com>
2024-05-25 18:07:04 -0500

    Created URL constants file
    * Create URL constants file
    * Add BASE_URL to constants file
    * Add berry urls to constants file
    * Create constants index.ts file

---
+++ server/constants/url.constants.ts
@@ -0,0 +1,6 @@
+const BASE_URL = "https://pokeapi.co/api/v2";
+
+// Berry urls
+export const BERRY_URL = `${BASE_URL}/berry`;
+export const BERRY_FIRMNESS_URL = `${BASE_URL}/berry-firmness`;
+export const BERRY_FLAVOR_URL = `${BASE_URL}/berry-flavor`;
